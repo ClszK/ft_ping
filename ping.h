45c4d2123aa44284a4b58489699618a0a7e7de18
clszk <dlwpgns0@gmail.com>
2025-01-26 13:56:26 +0000

    ft_ping: Add header

--- ping.h
+++ ping.h
@@ -2,16 +2,52 @@
 #define PING_H
 
 #include <arpa/inet.h>
+#include <errno.h>
 #include <netdb.h>
+#include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int is_valid_ipv4(const char *ip);
-unsigned short checksum(void *b, int len);
+typedef int bool;
+
+/**
+ * @brief 문자열의 ip가 옳은지 판별 함수.
+ *
+ * @param ip 문자열 ip
+ * @return bool
+ */
+bool is_valid_ipv4(const char* ip);
+
+/**
+ * @brief packet의 checksum 계산해주는 함수.
+ *
+ * @param b
+ * @param len
+ * @return unsigned short
+ */
+unsigned short checksum(void* b, int len);
+void error_exit(const char* msg, int exit_code);
+
+int setup_socket();
+/**
+ * @brief 주어진 호스트 이름 or IP를 확인하고
+ *
+ * @param host
+ * @param dest
+ * @return int
+ */
+int resolve_host(const char* host,
+                 struct sockaddr_in* dest);
+
+void create_icmp_packet(char* packet, int seq,
+                        int payload_size);
+int receive_ping_reply(int sock, pid_t id, int seq,
+                       struct timeval* rtt);
 
 #endif
